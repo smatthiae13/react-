ee2ab671e70d87dcc9259cda5e627c0d913a1550
Sam Matthiae <rocketsam13@yahoo.com>
2019-06-06 11:03:35 -0500

    adding this.setState

--- src/App.js
+++ src/App.js
@@ -11,13 +11,27 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    // console.log('Was clicked');
+    // DONT DO THIS this.state.persons[0].name = "Maximillian";
+    this.setState({
+      persons: [
+        {name: "Maximillian", age: 31},
+        {name: "Sam", age: 29},
+        {name: "Frederica", age: 32}
+      ]
+    })
+    // this method allows us to update / merge
+  }
+  // assigned a function to a property
 
   render() {
     return (
       <div className="App">
        <h1>Hi, I am a React app</h1>
-       <button>Switch Name</button>
-       <Person name={this.state.persons[0].name} age={this.state.persons[0].name} />
+       <button onClick={this.switchNameHandler}>Switch Name</button>
+   {/* dont add perenthesis after switchNameHandler, it will render immediately */}
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].name} />
        <Person name={this.state.persons[1].name} age={this.state.persons[1].name} >My Hobbies: Racing</Person>
        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
        {/* this refers to the class */}
