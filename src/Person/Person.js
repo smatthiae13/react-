fe6718f0109487ce038d9fd2614de2fcdef5330d
Sam Matthiae <rocketsam13@yahoo.com>
2019-06-08 13:42:12 -0500

    props and changed

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,7 +6,9 @@ const person = (props) => {
     return   (
         <div>
              <p onClick={props.click}>I am {props.name} and I am {props.age}</p>
-    <p>{props.children}</p>
+             <p>{props.children}</p>
+             <input type="text" onChange={props.changed} />
+
 </div>
 )
 };
