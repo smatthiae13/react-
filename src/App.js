12621249c1d672cf4ec94062fd773cc712fc8a02
Sam Matthiae <rocketsam13@yahoo.com>
2019-07-07 18:14:03 -0500

    adding list#55 in course

--- src/App.js
+++ src/App.js
@@ -1,71 +1,93 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
+
 
 class App extends Component {
   state = {
     persons: [
-      {name: "Max", age: 31},
-      {name: "Sam", age: 29},
-      {name: "Fred", age: 32}
-    ]
-  }
+      { name: "Max", age: 31 },
+      { name: "Sam", age: 29 },
+      { name: "Fred", age: 32 }
+    ],
+    otherState: "some other value",
+    showPersons: false        //doesnt show the persons
+  };
+//the persons area here is our data source - larger apps would fetch data from a server here
 
-  switchNameHandler = (newName) => {
+
+  switchNameHandler = newName => {
     // console.log('Was clicked');
     // DONT DO THIS this.state.persons[0].name = "Maximillian";
     this.setState({
       persons: [
-        {name: newName, age: 31},
-        {name: "Sam", age: 29},
-        {name: "Frederica", age: 32}
-      ]
-    })
+        { name: newName, age: 31 },
+        { name: "Sam", age: 29 },
+        { name: "Frederica", age: 32 }
+      ],
+      otherState: "some other value",
+      showPersons: false
+    });
     // this method allows us to update / merge
-  }
+  };
 
   // assigned a function to a property
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = event => {
     this.setState({
       persons: [
-        {name: "Max", age: 31},
-        {name: event.target.value, age: 29},
-        {name: "Frederica", age: 32}
+        { name: "Max", age: 31 },
+        { name: event.target.value, age: 29 },
+        { name: "Frederica", age: 32 }
       ]
-  })
-}
+    });
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
 
   render() {
-const style = {
-  backgroundColor: "white",
-  font: "inherit",
-  border: "1px solid blue",
-  padding: "8px",
-  cursor: "pointer"
-  //these have to have quotes because they are javascript
-};
+    const style = {
+      backgroundColor: "white",       //these have to have quotes because they are javascript
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+      
+    };
+
+    let persons = null; //this should be default
+
+    //in here it is all jscript, not jsx        //showPersons id boolean
+
+  //function inside map will execute on every persons array
+  //map makes the javascript work for jsx (i think this the right explanation)
+  //return what i want to map item into: returns a new array(a jscript array with jsx objects in it tp render to screen)
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map(person => {
+            return <Person 
+            name={person.name} 
+            age={person.age} />
+          })}
+          
+        </div>
+      );
+    }
+
     return (
       <div className="App">
-       <h1>Hi, I am a React app</h1>
-       <button 
-       style={style} //returns the style variable from above
-       onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button> 
-
-   {/* dont add perenthesis after switchNameHandler, it will render immediately */}
-      <Person 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].name} />
-       <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].name} 
-        click= {this.switchNameHandler.bind(this, "Max!")} 
-        changed={this.changedNameHandler} >My Hobbies: Racing</Person>
-        
-       <Person 
-        name={this.state.persons[2].name} 
-        age={this.state.persons[2].age} />
-       {/* this refers to the class */}
+        <h1>Hi, I am a React app</h1>
+        <button
+          style={style} //returns the style variable from above
+          onClick={() => this.togglePersonsHandler("Maximilian!!")}
+        >
+          Toggle Persons
+        </button>
+        {persons}
       </div>
     );
   }
@@ -73,4 +95,11 @@ const style = {
 
 export default App;
 
+
 //line 32 can be inefficient, bind is a better use
+
+// {/* dont add perenthesis after switchNameHandler, it will render immediately */ }
+
+//remember react is javascript
+
+
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import './Person.css';
-
- 
-//dont need a component here because we are not using a class to extend a component, 
-//we are creating a function
+import Person from "./Person.css";
 
 const person = (props) => {
-    return   (
+    return (
         <div className="Person">
-             <p onClick={props.click}>I am {props.name} and I am {props.age}</p>
-             <p>{props.children}</p>
-             <input type="text" onChange={props.changed} value={props.name}/>
-
-</div>
-)
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old! </p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </div> 
+    )
 };
-// use as many of these state less - presentational - dumb - pure functional components
-// as possible, makes coding easier
 
 export default person;
